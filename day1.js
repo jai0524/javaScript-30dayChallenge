5b97214711a0a8e3cfbbc3c64c62c071f7ad1edf
JaiPrakash24 <jaiprakash98768@gmail.com>
2025-03-26 20:45:34 +0530

    change

---
+++ day1.js
@@ -0,0 +1,45 @@
+// task 1 
+// declare the var and consloe this var
+var number = 5;
+console.log(number);
+
+// task 2
+var st= "hello";
+console.log(st);
+
+// task 3
+const boole = true;
+console.log(!boole);
+
+// task 4
+let a= 1;
+let b= "hello";
+let c=true;
+let d={
+    age:18,
+    class:10
+};
+let animal=[
+    "cat",
+    "dog",
+    "elephant"
+]
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(animal))
+
+//  tast 5
+let value = 5;
+ console.log(value);
+
+value = 4;
+console.log(value);
+
+// tast 6
+const x=5;
+console.log(x);
+ x=4;
+console.log(x);
