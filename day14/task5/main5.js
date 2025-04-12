b452f0b044dc8f313b5312614dbad87d12bb3af7
jai0524 <jaiprakash98768@gmail.com>
2025-04-12 09:25:56 +0000

    hi

--- day14/task5/main5.js
+++ day14/task5/main5.js
@@ -1,6 +1,6 @@
-import * as math from './mathUtils.js';
+import * as math from "./mathUtils.js";
 
-console.log("PI:", math.PI);                    // Output: PI: 3.14159
-console.log("E:", math.E);                      // Output: E: 2.718
-console.log("Add:", math.add(5, 3));            // Output: Add: 8
-console.log("Multiply:", math.multiply(4, 6));  // Output: Multiply: 24
+console.log("PI:",math.PI);
+console.log("E:", math.E);
+console.log("ADD :",math.add(25,3))
+console.log("MULTIPLY :",math.multiply(5,15))
--- day14/task5/mathUtils.js
+++ day14/task5/mathUtils.js
@@ -1,12 +1,12 @@
-// mathUtils.js
 
-export const PI = 3.14159;
-export const E = 2.718;
 
-export function add(a, b) {
-  return a + b;
+export const PI =3.14;
+export const E=2.55;
+
+export function add(a,b) {
+    return a+b;
+}
+export function multiply(a,b){
+    return a*b;
 }
 
-export function multiply(a, b) {
-  return a * b;
-}
